d3e00ec6f85a3dc9fd430915c2455f7ab1af3018
AK <akrivit@gmail.com>
2022-08-17 15:38:22 -0400

    progress was made

--- index.js
+++ index.js
@@ -2,11 +2,15 @@ const u = require('./utils.js');
 const path = require('path');
 const fs = require('fs').promises
 
-async function main(rawFile, metaDatDir) {	
-	const metaFiles = await u.listFiles((path.resolve(metaDatDir)));
-	const headers = await u.getHeaders(metaFiles.column_headers);
-	const rawParsed = await u.parseRaw(path.resolve(rawFile), headers);
-	const metaLookup = await u.getLookups(metaFiles);
+async function main(z_rawFile, z_metaDatDir, z_eVarDfns) {	
+	const A_metaFiles = await u.listFiles((path.resolve(z_metaDatDir)));
+	const B_headers = await u.getHeaders(A_metaFiles.column_headers);
+	const C_rawParsed = await u.parseRaw(path.resolve(z_rawFile), B_headers);
+	const D_metaLookup = await u.getLookups(A_metaFiles);
+	const E_joinMetaAndRaw = await u.applyLookups(C_rawParsed, D_metaLookup);
+	const F_evarSource = await u.loadTSV(path.resolve(z_eVarDfns), true);
+	const G_dataWithEvar = await u.applyEvars(E_joinMetaAndRaw, F_evarSource);
+	const H_dataSansEmpties = u.noNulls(G_dataWithEvar)
 
 	debugger;
 	return 42
--- utils.js
+++ utils.js
@@ -1,7 +1,9 @@
+// https://experienceleague.adobe.com/docs/analytics/export/analytics-data-feed/data-feed-contents/datafeeds-reference.html?lang=en
+
 const path = require('path')
 const fs = require('fs').promises
 const parseTSV = require('papaparse')
-const { zipObject } = require('lodash')
+const { zipObject, mapKeys } = require('lodash')
 
 exports.listFiles = async function (dir) {
     let fileList = await fs.readdir(dir);
@@ -37,5 +39,122 @@ exports.getLookups = async function(metas = {}) {
 		result[key] = lookup
 	}
 
+	//hacky shit i found when spot checking; lookups + raw do not share named keys
+	result.search_engine = result.search_engines
+	delete result.search_engines
+
+	result.ref_type = result.referrer_type
+	delete result.referrer_type
+
+	result.os = result.operating_systems
+	delete result.operating_systems
+
+	result.language = result.languages
+	delete result.languages
+
+	result.javascript = result.javascript_version
+	delete result.javascript_version
+
+	result.event_list = result.event
+	delete result.event
+
+	//moar hacky shit for transformation resolution
+	const viewports = result.resolution
+	result.browser_width = viewports.map((pair) => [pair[0], pair[1]?.split('x')?.[0]?.trim() || pair[1]]);
+	result.browser_height = viewports.map((pair) => [pair[0], pair[1]?.split('x')?.[1]?.trim() || pair[1]]);	
+	delete result.resolution
+	
+
 	return result
+}
+
+exports.loadTSV = async function(filePath, hasHeaders = false) {
+	let file = await fs.readFile(filePath, 'utf8');
+    return parseTSV.parse(file, {header: hasHeaders}).data;
+}
+
+exports.applyLookups = async function(raw, lookups) {	
+	for (const event of raw) {
+		for (let key in event) {
+			if (lookups[key]) {
+				let ogValue = event[key];
+				let pair = lookups[key].filter(pair => pair.includes(ogValue)).flat()
+				if (pair.length > 0) {
+					let newValue = pair[1]
+					event[key] = newValue
+				}
+				
+			}
+		}
+	}
+
+	return raw;
+}
+
+exports.applyEvars = async function(raw, evars) {
+	//rename the evar keys
+	const keyMap = {};
+	for (const evar of evars) {
+		keyMap[`post_${evar.id.split('/')[1]}`] = evar.name
+	}
+
+	const appliedEvars = raw.map((event)=>{
+		return mapKeys(event, (value, key)=>{
+			if (keyMap[key]) {
+				return keyMap[key]
+			}
+			else {
+				return key
+			}
+		})
+	})
+
+	return appliedEvars;
+}
+
+exports.noNulls = function (arr = []) {	
+	for (let thing of arr) {
+		removeNulls(thing)
+	}
+
+	return arr
+}
+
+
+const removeNulls = function (obj) {
+    function isObject(val) {
+        if (val === null) { return false; }
+        return ((typeof val === 'function') || (typeof val === 'object'));
+    }
+
+    const isArray = obj instanceof Array;
+
+    for (var k in obj) {
+        // falsy values
+        if (!Boolean(obj[k])) {
+            isArray ? obj.splice(k, 1) : delete obj[k]
+        }
+
+		//empty strings
+		if (obj[k] === "") {
+			delete obj[k]
+		}
+
+        // empty arrays
+        if (Array.isArray(obj[k]) && obj[k]?.length === 0) {
+            delete obj[k]
+        }
+
+        // empty objects
+        if (isObject(obj[k])) {
+            if (JSON.stringify(obj[k]) === '{}') {
+                delete obj[k]
+            }
+        }
+
+        // recursion
+        if (isObject(obj[k])) {
+            removeNulls(obj[k])
+        }
+    }
 }
