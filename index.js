5fcc3ac008fea64389da1808142241395735f74e
AK <akrivit@gmail.com>
2022-08-17 23:57:58 -0400

    foolish progress

--- index.js
+++ index.js
@@ -1,16 +1,38 @@
 const u = require('./utils.js');
 const path = require('path');
 const fs = require('fs').promises
+const mpImport = require('mixpanel-import');
 
-async function main(z_rawFile, z_metaDatDir, z_eVarDfns) {	
+async function main(z_rawFile, z_metaDatDir, z_eVarDfns, mpCreds) {	
+	// PARSE AND CLEAN
 	const A_metaFiles = await u.listFiles((path.resolve(z_metaDatDir)));
 	const B_headers = await u.getHeaders(A_metaFiles.column_headers);
 	const C_rawParsed = await u.parseRaw(path.resolve(z_rawFile), B_headers);
-	const D_metaLookup = await u.getLookups(A_metaFiles);
-	const E_joinMetaAndRaw = await u.applyLookups(C_rawParsed, D_metaLookup);
-	const F_evarSource = await u.loadTSV(path.resolve(z_eVarDfns), true);
-	const G_dataWithEvar = await u.applyEvars(E_joinMetaAndRaw, F_evarSource);
-	const H_dataSansEmpties = u.noNulls(G_dataWithEvar)
+	const D_dataSansEmpties = u.noNulls(C_rawParsed);
+
+	// APPLY ADOBE LOOKUPS
+	const E_metaLookup = await u.getLookups(A_metaFiles);
+	const F_joinMetaAndRaw = await u.applyLookups(D_dataSansEmpties, E_metaLookup);
+	const G_evarSource = await u.loadTSV(path.resolve(z_eVarDfns), true);
+	const H_dataWithEvar = await u.applyEvars(F_joinMetaAndRaw, G_evarSource);	
+	
+	//TRANSFORM AND SEND TO MP
+	const I_transformToMp = u.adobeToMp(H_dataWithEvar)
+	
+	const options = {
+		recordType: `event`, //event, user, OR group
+		streamSize: 27, // highWaterMark for streaming chunks (2^27 ~= 134MB)
+		region: `US`, //US or EU
+		recordsPerBatch: 2000, //max # of records in each batch
+		bytesPerBatch: 2 * 1024 * 1024, //max # of bytes in each batch
+		strict: true, //use strict mode?
+		logs: true, //print to stdout?
+	
+		//a function reference to be called on every record
+		//useful if you need to transform the data
+		transformFunc: function noop(a) { return a }
+	}
+	// const importedData = await mpImport(creds, data, options);
 
 	debugger;
 	return 42
--- utils.js
+++ utils.js
@@ -4,8 +4,9 @@ const path = require('path')
 const fs = require('fs').promises
 const parseTSV = require('papaparse')
 const { zipObject, mapKeys } = require('lodash')
+const md5 = require('md5')
 
-exports.listFiles = async function (dir) {
+exports.listFiles = async function (dir = "./") {
     let fileList = await fs.readdir(dir);
     let results = {};
     for (fileName of fileList) {
@@ -14,112 +15,156 @@ exports.listFiles = async function (dir) {
     return results
 }
 
-exports.getHeaders = async function (filePath) {
+exports.getHeaders = async function (filePath = "./") {
     let file = await fs.readFile(filePath, 'utf8');
     let headers = parseTSV.parse(file).data[0];
     return headers;
 }
 
-exports.parseRaw = async function (rawPath, headers) {
+exports.parseRaw = async function (rawPath = "./", headers = []) {
     let rawData = await fs.readFile(rawPath, 'utf-8');
     let parseRaw = parseTSV.parse(rawData).data;
-	let labeled = parseRaw.map((row)=>{
-		return zipObject(headers, row)
-	})
+    let labeled = parseRaw.map((row) => {
+        return zipObject(headers, row)
+    })
     return labeled;
 }
 
-exports.getLookups = async function(metas = {}) {
-	//don't need col headers; already got 'em
-	delete metas.column_headers;
+exports.getLookups = async function (metas = {}) {
+    //don't need col headers; already got 'em
+    delete metas.column_headers;
 
-	let result = {};		
-	for (let key in metas) {
-		let lookup = parseTSV.parse(await fs.readFile(metas[key], 'utf-8')).data
-		result[key] = lookup
-	}
+    let result = {};
+    for (let key in metas) {
+        let lookup = parseTSV.parse(await fs.readFile(metas[key], 'utf-8')).data
+        result[key] = lookup
+    }
 
-	//hacky shit i found when spot checking; lookups + raw do not share named keys
-	result.search_engine = result.search_engines
-	delete result.search_engines
+    //hacky shit i found when spot checking; lookups + raw do not share named keys
+    result.search_engine = result.search_engines
+    delete result.search_engines
 
-	result.ref_type = result.referrer_type
-	delete result.referrer_type
+    result.ref_type = result.referrer_type
+    delete result.referrer_type
 
-	result.os = result.operating_systems
-	delete result.operating_systems
+    result.os = result.operating_systems
+    delete result.operating_systems
 
-	result.language = result.languages
-	delete result.languages
+    result.language = result.languages
+    delete result.languages
 
-	result.javascript = result.javascript_version
-	delete result.javascript_version
+    result.javascript = result.javascript_version
+    delete result.javascript_version
 
-	result.event_list = result.event
-	delete result.event
+    result.event_list = result.event
+    delete result.event
 
-	//moar hacky shit for transformation resolution
-	const viewports = result.resolution
-	result.browser_width = viewports.map((pair) => [pair[0], pair[1]?.split('x')?.[0]?.trim() || pair[1]]);
-	result.browser_height = viewports.map((pair) => [pair[0], pair[1]?.split('x')?.[1]?.trim() || pair[1]]);	
-	delete result.resolution
-	
+    //moar hacky shit for transformation resolution
+    const viewports = result.resolution
+    result.browser_width = viewports.map((pair) => [pair[0], pair[1]?.split('x')?.[0]?.trim() || pair[1]]);
+    result.browser_height = viewports.map((pair) => [pair[0], pair[1]?.split('x')?.[1]?.trim() || pair[1]]);
+    delete result.resolution
 
-	return result
+
+    return result
 }
 
-exports.loadTSV = async function(filePath, hasHeaders = false) {
-	let file = await fs.readFile(filePath, 'utf8');
-    return parseTSV.parse(file, {header: hasHeaders}).data;
+exports.loadTSV = async function (filePath = "", hasHeaders = false) {
+    let file = await fs.readFile(filePath, 'utf8');
+    return parseTSV.parse(file, { header: hasHeaders }).data;
 }
 
-exports.applyLookups = async function(raw, lookups) {	
-	for (const event of raw) {
-		for (let key in event) {
-			if (lookups[key]) {
-				let ogValue = event[key];
-				let pair = lookups[key].filter(pair => pair.includes(ogValue)).flat()
-				if (pair.length > 0) {
-					let newValue = pair[1]
-					event[key] = newValue
-				}
-				
-			}
-		}
-	}
+exports.applyLookups = async function (raw = [], lookups = {}) {
+    const target = clone(raw)
+    for (const event of target) {
+        for (let key in event) {
+            if (lookups[key]) {
+                let ogValue = event[key];
+                let pair = lookups[key].filter(pair => pair[0] === ogValue).flat()
+                if (pair.length > 0) {
+                    let newValue = pair[1]
+                    event[key] = newValue
+                }
+            }
 
-	return raw;
+            //resolve event names
+            if (key === `post_event_list` && event[key]) {
+                const allEventsInSession = event[key].split(',');
+                const eventNameLookup = allEventsInSession.map((evId) => {
+                    const resolvedEvent = lookups.event_list.filter(pair => pair[0] === evId).flat()[1]
+                    return resolvedEvent
+                })
+
+                event.post_event_list_resolved = eventNameLookup;
+            }
+        }
+    }
+
+    return target;
 }
 
-exports.applyEvars = async function(raw, evars) {
-	//rename the evar keys
-	const keyMap = {};
-	for (const evar of evars) {
-		keyMap[`post_${evar.id.split('/')[1]}`] = evar.name
-	}
+exports.applyEvars = async function (raw = [], evars) {
+    //rename the evar keys
+    const keyMap = {};
+    for (const evar of evars) {
+        keyMap[`post_${evar.id.split('/')[1]}`] = evar.name
+    }
 
-	const appliedEvars = raw.map((event)=>{
-		return mapKeys(event, (value, key)=>{
-			if (keyMap[key]) {
-				return keyMap[key]
-			}
-			else {
-				return key
-			}
-		})
-	})
+    const target = clone(raw)
 
-	return appliedEvars;
+    const appliedEvars = target.map((event) => {
+        return mapKeys(event, (value, key) => {
+            if (keyMap[key]) {
+                return keyMap[key]
+            } else {
+                return key
+            }
+        })
+    })
+
+    return appliedEvars;
 }
 
-exports.noNulls = function (arr = []) {	
-	for (let thing of arr) {
-		removeNulls(thing)
-	}
+exports.noNulls = function (arr = []) {
+    let target = clone(arr)
+    for (let thing of target) {
+        removeNulls(thing)
+    }
 
-	return arr
+    return target
 }
 
+exports.adobeToMp = function (events = []) {
+    const transform = events.map((adobe) => {
+        //this is garbage
+        let eventName;
+        try {
+            eventName = adobe.post_event_list_resolved[0]
+        } catch (e) {
+            eventName = `unknown`
+        }
+
+        let mp = {
+            event: eventName,
+            properties: {
+                time: Number(adobe.visit_start_time_gmt),
+                distinct_id: `${adobe.post_visid_high}${adobe.post_visid_low}`,
+                ...adobe
+            }
+        }
+        mp = addInsert(mp)
+        return mp
+
+    })
+
+    return transform;
+}
+
+const addInsert = function (event) {
+    let hash = md5(event);
+    event.properties.$insert_id = hash;
+    return event
+}
 
 const removeNulls = function (obj) {
     function isObject(val) {
@@ -135,10 +180,10 @@ const removeNulls = function (obj) {
             isArray ? obj.splice(k, 1) : delete obj[k]
         }
 
-		//empty strings
-		if (obj[k] === "") {
-			delete obj[k]
-		}
+        //empty strings
+        if (obj[k] === "") {
+            delete obj[k]
+        }
 
         // empty arrays
         if (Array.isArray(obj[k]) && obj[k]?.length === 0) {
@@ -156,5 +201,35 @@ const removeNulls = function (obj) {
         if (isObject(obj[k])) {
             removeNulls(obj[k])
         }
+    }
+}
+
+const clone = function (thing, opts) {
+    var newObject = {};
+    if (thing instanceof Array) {
+        return thing.map(function (i) { return clone(i, opts); });
+    } else if (thing instanceof Date) {
+        return new Date(thing);
+    } else if (thing instanceof RegExp) {
+        return new RegExp(thing);
+    } else if (thing instanceof Function) {
+        return opts && opts.newFns ?
+            new Function('return ' + thing.toString())() :
+            thing;
+    } else if (thing instanceof Object) {
+        Object.keys(thing).forEach(function (key) {
+            newObject[key] = clone(thing[key], opts);
+        });
+        return newObject;
+    } else if ([undefined, null].indexOf(thing) > -1) {
+        return thing;
+    } else {
+        if (thing.constructor.name === 'Symbol') {
+            return Symbol(thing.toString()
+                .replace(/^Symbol\(/, '')
+                .slice(0, -1));
+        }
+        // return _.clone(thing);  // If you must use _ ;)
+        return thing.__proto__.constructor(thing);
     }
 }
